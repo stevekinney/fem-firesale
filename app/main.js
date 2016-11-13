0acc66c6ce356fc675aa928f706c1905d296cfa4
Steve Kinney <hello@stevekinney.net>
2016-11-13 13:57:35 -0700

    Get basic functionality in place

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
 
 let mainWindow = null;
 
@@ -8,7 +9,28 @@ app.on('ready', () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   require('devtron').install();
 
+  showOpenFileDialog();
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
+
+const showOpenFileDialog = exports.showOpenFileDialog = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt'] },
+      { name: 'Markdown Files', extensions: ['md', 'markdown'] }
+    ]
+  });
+
+  if (!files) return;
+
+  openFile(files[0]);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  mainWindow.webContents.send('file-opened', file, content);
+};
---
+++ app/renderer.js
@@ -0,0 +1,25 @@
+const { ipcRenderer, remote } = require('electron');
+const { showOpenFileDialog } = remote.require('./main');
+
+const marked = require('marked');
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const openFileButton = document.querySelector('#open-file');
+
+const renderMarkdownToHtml = (markdown) => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdownToHtml(event.target.value);
+});
+
+openFileButton.addEventListener('click', () => {
+  showOpenFileDialog();
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.textContent = content;
+  renderMarkdownToHtml(content);
+});
