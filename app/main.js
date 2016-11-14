45f7c346839022f342819785d4f7534e753bb2ba
Steve Kinney <hello@stevekinney.net>
2016-11-14 17:16:11 -0600

    Add the ability to save and open files from the icon

--- app/main.js
+++ app/main.js
@@ -3,18 +3,44 @@ const fs = require('fs');
 
 const windows = new Set();
 
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, file) => {
+    event.preventDefault();
+    createWindow(file);
+  });
+});
+
 app.on('ready', () => {
   createWindow();
 });
 
-const createWindow = exports.createWindow = () => {
+const createWindow = exports.createWindow = (file) => {
   let newWindow = new BrowserWindow({ show: false });
   windows.add(newWindow);
 
   newWindow.loadURL(`file://${__dirname}/index.html`);
 
   newWindow.once('ready-to-show', () => {
+    if (file) openFile(newWindow, file);
     newWindow.show();
+  });
+
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: 'Quit with Unsaved Changes?',
+        message: 'Your changes will be lost permanently if you do not save.',
+        buttons: [
+          'Quit Anyway',
+          'Cancel',
+        ],
+        cancelId: 1,
+        defaultId: 0
+      });
+
+      if (result === 0) newWindow.destroy();
+    }
   });
 
   newWindow.on('closed', () => {
@@ -39,8 +65,9 @@ const getFileFromUserSelection = exports.getFileFromUserSelection = (win) => {
   return files[0];
 };
 
-const openFile = exports.openFile = (win, file) => {
-  const f = file || getFileFromUserSelection(win);
-  const content = fs.readFileSync(f).toString();
+const openFile = exports.openFile = (win, file = getFileFromUserSelection(win)) => {
+  const content = fs.readFileSync(file).toString();
+  app.addRecentDocument(file);
   win.webContents.send('file-opened', file, content);
+  win.setRepresentedFilename(file);
 };
--- app/renderer.js
+++ app/renderer.js
@@ -8,13 +8,32 @@ const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
 const newFileButton = document.querySelector('#new-file');
 const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+
+let filePath = null;
+let originalContent = '';
 
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateEditedState = (isEdited) => {
+  currentWindow.setDocumentEdited(isEdited);
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  let title = 'Fire Sale';
+  if (filePath) title = `${filePath} - ${title}`;
+  if (isEdited) title = `${title} (Edited)`;
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', (event) => {
-  renderMarkdownToHtml(event.target.value);
+  const content = event.target.value;
+  renderMarkdownToHtml(content);
+  updateEditedState(content !== originalContent);
 });
 
 newFileButton.addEventListener('click', () => {
@@ -25,7 +44,18 @@ openFileButton.addEventListener('click', () => {
   openFile(currentWindow);
 });
 
+revertButton.addEventListener('click', () => {
+  markdownView.value = originalContent;
+  renderMarkdownToHtml(originalContent);
+  updateEditedState(false);
+});
+
 ipcRenderer.on('file-opened', (event, file, content) => {
-  markdownView.textContent = content;
+  filePath = file;
+  originalContent = content;
+
+  updateEditedState(false);
+
+  markdownView.value = content;
   renderMarkdownToHtml(content);
 });
