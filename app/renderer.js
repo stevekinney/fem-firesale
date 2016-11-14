8642a148ade1456613f1896965444133676283ae
Steve Kinney <hello@stevekinney.net>
2016-11-13 17:52:49 -0700

    Add cached selectors

---
+++ app/renderer.js
@@ -0,0 +1,9 @@
+const marked = require('marked');
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
