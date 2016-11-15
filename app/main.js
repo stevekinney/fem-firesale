298818c46d00f93139b7e9a09a57c5abc21bf1bc
Steve Kinney <hello@stevekinney.net>
2016-11-15 14:03:44 -0600

    Live coding before break

--- app/main.js
+++ app/main.js
@@ -1,10 +1,45 @@
 const { app, BrowserWindow, dialog } = require('electron');
 const fs = require('fs');
 
-let mainWindow = null;
+const windows = new Set();
 
-const getFileFromUserSelection = exports.getFileFromUserSelection = () => {
-  const files = dialog.showOpenDialog(mainWindow, {
+const createWindow = exports.createWindow = () => {
+  let newWindow = new BrowserWindow({ show: false });
+  windows.add(newWindow);
+
+  newWindow.loadURL(`file://${__dirname}/index.html`);
+
+  newWindow.once('ready-to-show', () => {
+    newWindow.show();
+  });
+
+  newWindow.on('close', (event) => {
+    if (newWindow.isDocumentEdited()) {
+      event.preventDefault();
+      const result = dialog.showMessageBox(newWindow, {
+        type: 'warning',
+        title: 'Quick with Unsaved Changes?',
+        message: 'Your changes will be lost if you do not save first.',
+        buttons: [
+          'Quit Anyway',
+          'Cancel'
+        ],
+        defaultId: 1,
+        cancelId: 1
+      });
+
+      if (result === 0) newWindow.destroy();
+    }
+  });
+
+  newWindow.on('closed', () => {
+    windows.delete(newWindow);
+    newWindow = null;
+  });
+};
+
+const getFileFromUserSelection = exports.getFileFromUserSelection = (targetWindow) => {
+  const files = dialog.showOpenDialog(targetWindow, {
     properties: ['openFile'],
     filters: [
       { name: 'Text Files', extensions: ['txt', 'text'] },
@@ -14,22 +49,16 @@ const getFileFromUserSelection = exports.getFileFromUserSelection = () => {
 
   if (!files) return;
 
-  const file = files[0];
-  const content = fs.readFileSync(file).toString();
+  return files[0];
+};
 
-  mainWindow.webContents.send('file-opened', file, content);
+const openFile = exports.openFile = (targetWindow, filePath) => {
+  const file = filePath || getFileFromUserSelection(targetWindow);
+  const content = fs.readFileSync(file).toString();
+  targetWindow.webContents.send('file-opened', file, content);
+  targetWindow.setRepresentedFilename(file);
 };
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
-
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+  createWindow();
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,7 @@
 const marked = require('marked');
 const { remote, ipcRenderer } = require('electron');
 const mainProcess = remote.require('./main');
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -10,19 +11,45 @@ const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
 
+let filePath = null;
+let originalContent = '';
+
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateEditedState = (isEdited) => {
+  currentWindow.setDocumentEdited(isEdited);
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  let title = 'Fire Sale';
+  if (filePath) title = `${filePath} - ${title}`;
+  if (isEdited) title = `${title} (Edited)`;
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', (event) => {
-  renderMarkdownToHtml(event.target.value);
+  const currentContent = event.target.value;
+  renderMarkdownToHtml(currentContent);
+  updateEditedState(currentContent !== originalContent);
+});
+
+newFileButton.addEventListener('click', () => {
+  mainProcess.createWindow();
 });
 
 openFileButton.addEventListener('click', () => {
-  mainProcess.getFileFromUserSelection();
+  mainProcess.openFile(currentWindow);
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
   markdownView.value = content;
   renderMarkdownToHtml(content);
+
+  updateEditedState(false);
 });
